a9d4a39a4a37d00616067cfc163ea5fbbfdc4f97
Catalin Podariu <catalin.podariu@gmail.com>
2016-04-04 08:34:11 +0200

    Primary functionality for regex find

--- src/searchModes/RegEx.java
+++ src/searchModes/RegEx.java
@@ -1,5 +1,8 @@
 package searchModes;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.swing.text.BadLocationException;
 import javax.swing.text.StyledDocument;
 
@@ -9,6 +12,8 @@ public class RegEx {
 
 	private BIGNotepad notepad;
 	private StyledDocument document;
+	private Pattern serverPattern;
+	private Matcher matchServer;
 
 	private RegEx(BIGNotepad notepad) {
 		this.notepad = notepad;
@@ -19,8 +24,26 @@ public class RegEx {
 		return new RegEx(notepad);
 	}
 
-	public void find(String toFind, int searchMode) throws BadLocationException {
-		// TODO catalin.podariu REGEX Functionality
+	public void findNext(String toFind, String regexPattern) throws BadLocationException {
+		for (int index = notepad.currentSearchLocation; index < document.getLength(); index++) {
+			if (matchFound(toFind)) {
+				Feedback.valueOf(notepad).selectString(index, toFind.length());
+			}
+		}
+	}
+
+	public void findPrevious(String toFind) throws BadLocationException {
+		for (int index = notepad.currentSearchLocation; index - toFind.length() >= 0; index--) {
+			if (matchFound(toFind)) {
+				Feedback.valueOf(notepad).selectString(index, toFind.length());
+			}
+		}
+	}
+
+	public boolean matchFound(final String toFind) {
+		serverPattern = Pattern.compile(toFind);
+		matchServer = serverPattern.matcher(toFind);
+		return matchServer.matches();
 	}
 
 }
